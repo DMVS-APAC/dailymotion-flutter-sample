73ec727d8c3de9f11554daf3485e50674c6064a3
Arryangga Aliev Pratamaputra <arryanggaputra@gmail.com>
2022-11-21 15:11:43 +0700

    :construction: add demo how to use DailymotionPlayer v1 and v2

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'dart:async'; // Add this import
 
 import 'package:flutter/material.dart';
-import 'package:flutter_dm_player/navigation_controls.dart';
+import 'package:flutter_dm_player/DailymotionPlayer.dart';
+import 'package:flutter_dm_player/DailymotionPlayerV2.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:webview_flutter/webview_flutter.dart'; // Add this import back
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(
     const MaterialApp(
       home: WebViewApp(),
@@ -20,8 +24,16 @@ class WebViewApp extends StatefulWidget {
 }
 
 class _WebViewAppState extends State<WebViewApp> {
-  final controller =
-      Completer<WebViewController>(); // Instantiate the controller
+  late final WebViewController theController;
+  InAppWebViewController? theControllerv2;
+
+  final GlobalKey<DailymotionPlayerState> dmPlayer =
+      GlobalKey<DailymotionPlayerState>();
+
+  final GlobalKey<DailymotionPlayerStateV2> dmPlayer2 =
+      GlobalKey<DailymotionPlayerStateV2>();
+
+  double playerHeight = 200;
 
   @override
   Widget build(BuildContext context) {
@@ -29,18 +41,72 @@ class _WebViewAppState extends State<WebViewApp> {
       appBar: AppBar(
         title: const Text('Dailymotion Player'),
         centerTitle: false,
-        actions: [
-          NavigationControls(controller: controller),
-        ],
       ),
-      body: WebView(
-        initialUrl: 'https://MintyHoneydewInstitutes.arryanggaputra.repl.co',
-        onWebViewCreated: (webViewController) {
-          controller.complete(webViewController);
-        },
-        javascriptMode: JavascriptMode.unrestricted,
-        allowsInlineMediaPlayback: true,
-        initialMediaPlaybackPolicy: AutoMediaPlaybackPolicy.always_allow,
+      body: Column(
+        children: [
+          DailymotionPlayer(
+            playerId: "xathg",
+            videoId: "x8fl3xz",
+            key: dmPlayer,
+            height: playerHeight,
+            onLoaded: (controller) {
+              print('loaded');
+            },
+            onPageFinished: (p0) => {print("finish loaded")},
+          ),
+          Row(
+            children: <Widget>[
+              IconButton(
+                icon: const Icon(Icons.play_arrow),
+                onPressed: () => {dmPlayer.currentState?.play()},
+              ),
+              IconButton(
+                icon: const Icon(Icons.pause),
+                onPressed: () => {dmPlayer.currentState?.pause()},
+              ),
+              IconButton(
+                icon: const Icon(Icons.replay_outlined),
+                onPressed: () => {dmPlayer.currentState?.reload()},
+              ),
+              IconButton(
+                icon: const Icon(Icons.fullscreen),
+                onPressed: () => {
+                  setState(() {
+                    playerHeight = 500;
+                  })
+                },
+              ),
+            ],
+          ),
+          // DailymotionPlayerV2(
+          //   playerId: "xathg",
+          //   videoId: "x8fl3xz",
+          //   key: dmPlayer2,
+          //   onLoaded: (controller) {
+          //     print('loaded');
+          //   },
+          // ),
+          // Row(
+          //   children: <Widget>[
+          //     IconButton(
+          //       icon: const Icon(Icons.play_arrow),
+          //       onPressed: () => {dmPlayer2.currentState?.play()},
+          //     ),
+          //     IconButton(
+          //       icon: const Icon(Icons.pause),
+          //       onPressed: () => {dmPlayer2.currentState?.pause()},
+          //     ),
+          //     IconButton(
+          //       icon: const Icon(Icons.replay_outlined),
+          //       onPressed: () => {dmPlayer2.currentState?.reload()},
+          //     ),
+          //     IconButton(
+          //       icon: const Icon(Icons.fullscreen),
+          //       onPressed: () => {dmPlayer2.currentState?.fullScreen()},
+          //     ),
+          //   ],
+          // )
+        ],
       ),
     );
   }
